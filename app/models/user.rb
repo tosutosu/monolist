59b72ebe28696d241cb26a51105a5f894599398c
chidatoshihiro <toshi.c.1986@gmail.com>
2015-08-25 19:40:56 +0000

    Mentor check. git diff app/models/item.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,14 @@ class User < ActiveRecord::Base
 
   has_many :ownerships , foreign_key: "user_id", dependent: :destroy
   has_many :items ,through: :ownerships
-
+  
+  ## begin 確認してほしいコード１
+  has_many :wants, class_name: "Want", foreign_key: "user_id", dependent: :destroy
+  has_many :want_items, through: :wants, source: :item
+  
+  has_many :haves, class_name: "Have", foreign_key: "user_id", dependent: :destroy
+  has_many :have_items, through: :haves, source: :item
+  ## end 確認してほしいコード１
 
   # 他のユーザーをフォローする
   def follow(other_user)
@@ -30,21 +37,29 @@ class User < ActiveRecord::Base
   end
 
   ## TODO 実装
+  ## begin 確認してほしいコード２
   def have(item)
+    haves.create(user_id: item.id)
   end
 
   def unhave(item)
+    haves.find_by(user_id: item.id).destroy
   end
 
   def have?(item)
+    have_items.include?(item)
   end
 
   def want(item)
+    wants.create(user_id: :item)
   end
 
   def unwant(item)
+    wants.find_by(user_id: :item).destroy
   end
 
   def want?(item)
+    want_items.include?(item)
   end
+  ## end 確認してほしいコード２
 end
